988f31448bfe7e5dd0ba7e20ff87f444c6eaf314
NicholasPaniccioli <njp7745@g.rit.edu>
2021-05-29 17:19:20 -0400

    Spelling and Comments
    Added comments for readability, and fixed spelling of a word

--- src/projectHandler.js
+++ src/projectHandler.js
@@ -1,6 +1,9 @@
+//Gives back the appropriate info based on selected project
 function getProject (){
+    //Gets the project
     const projList = document.querySelector("#projList").value;
 
+    //Checks which one and gives documentation
     if(projList === "OSE")
     {
         let desc = document.querySelector("#projDesc");
@@ -10,6 +13,7 @@ function getProject (){
         let desc = document.querySelector("#projDesc");
         desc.innerHTML = "LF project desc, testing";
     } else {
+        //If none is found, default message
         let desc = document.querySelector("#projDesc");
         desc.innerHTML = "could not find";
     }
