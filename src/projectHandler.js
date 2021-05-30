f67e5f2517d5033916a4576c39f75ee6b6c3ebc7
NicholasPaniccioli <njp7745@g.rit.edu>
2021-05-30 00:24:22 -0400

    Added Desc to Landmark Finder
    I added a description and picture to the Landmark Finder documentation.

--- src/projectHandler.js
+++ src/projectHandler.js
@@ -22,7 +22,17 @@ function getProject (){
 //Landmark Finder
 function getProjLandmark (desc, thumbnail){
     thumbnail.src = "images/mapboxAPI.JPG";
-    desc.innerHTML = "LF project desc, testing";
+    desc.innerHTML = "This project was originally created as a school project back in April 2020." +
+    " For a month I had to program a site that used Two APIs together to create something useful." +
+    " So I designed my own API using PHP to organize and return Landmarks based on user input." +
+    " Then combined with the MapBox API, I was able to map out and give back the location of those" +
+    " landmarks." + 
+    "<br><br>" +
+    "While I was unable to finish the project, the final product would've included functionality" +
+    " such as facts and pictures of the landmarks showing up, more ways to filter through landmarks," +
+    " and more ways to interact with the map." +
+    "<br><br>" +
+    "Overall the project deals in HTML/CSS, JSON, PHP, and APIs.";
 };
 
 //Outer Space Exprience
