a85996dbf8750ef3bb77ff7b57f458f20d40ce14
NicholasPaniccioli <njp7745@g.rit.edu>
2021-05-29 23:05:14 -0400

    Update Project Documentation
    Alphabetized  projects, added JS functionality to change html on selected project. Also added link on sources page. CSS added for thumbnail

--- src/projectHandler.js
+++ src/projectHandler.js
@@ -2,19 +2,31 @@
 function getProject (){
     //Gets the project
     const projList = document.querySelector("#projList").value;
+    let desc = document.querySelector("#projDesc");
+    let thumbnail = document.querySelector("#projPic");
 
-    //Checks which one and gives documentation
-    if(projList === "OSE")
-    {
-        let desc = document.querySelector("#projDesc");
-        desc.innerHTML = "OSE project desc, testing";
-    } else if(projList === "LF")
-    {
-        let desc = document.querySelector("#projDesc");
-        desc.innerHTML = "LF project desc, testing";
+    //Finds the project selected and gives documentation
+    //PROJECTS IN ALPHABETICAL ORDER
+    if(projList === "LF") {
+        //Landmark Finder
+        getProjLandmark(desc, thumbnail);
+    } else if(projList === "OSE") {
+        //Outer Space Experience
+        getProjSpace(desc, thumbnail);
     } else {
         //If none is found, default message
-        let desc = document.querySelector("#projDesc");
         desc.innerHTML = "could not find";
     }
+};
+
+//Landmark Finder
+function getProjLandmark (desc, thumbnail){
+    thumbnail.src = "images/mapboxAPI.JPG";
+    desc.innerHTML = "LF project desc, testing";
+};
+
+//Outer Space Exprience
+function getProjSpace (desc, thumbnail){
+    thumbnail.src = "images/spaceExp.JPG";
+    desc.innerHTML = "OSE project desc, testing";
 };
