147c9cdefc448dab6b45358644e6278f14a59f23
NicholasPaniccioli <njp7745@g.rit.edu>
2021-07-11 19:16:08 -0400

    More functionality
    Added start button to game.html and added functionality and more mechanics to the gameController

--- src/guess game/gameController.js
+++ src/guess game/gameController.js
@@ -1,5 +1,35 @@
-//Starts the game, picks random number, and adds appropriate html elements
+let numToGuess;
+
+//Starts the game, adds appropriate html elements, and picks a random number
 function startGame() {
+    setUpHTML();
+    
+    let gameText = document.querySelector("#gameText");
+    gameText.innerHTML = "Game has started and the number has been picked. Time to Guess!"
+
+    numToGuess = randNum();
+    console.log(numToGuess);
+}
+
+//Creates a new random number
+function restartGame(){
+    numToGuess = randNum();
+    console.log(numToGuess);
+}
+
+//Returns a random number bewtween 1 and 100
+function randNum () {
+    return Math.floor((Math.random() * 100) + 1);
+}
+
+//Sets up the HTML for the game
+function setUpHTML(){
+
+    //Updates start button to now be a restart
+    //Changes functionality to prevent additional html to be added
+    let resetButton = document.querySelector("#startButton");
+    resetButton.value = "Restart";
+    resetButton.onclick = restartGame;
 
     //Grabs the div where the created elemetns will be stored
     let gameControls = document.querySelector("#gameControls");
@@ -22,18 +52,12 @@ function startGame() {
     //Adds to Submit
     guessSubmit.innerHTML = "Guess";
     guessSubmit.type = "button";
-    guessSubmit.onclick = "logged()";
+    //Gives test function to onclick on submit button
+    guessSubmit.onclick = guess;
 }
 
-// This is being used for testing purposes
-function logged () {
-
+function guess() {
     let gameText = document.querySelector("#gameText");
     let numGuess = document.querySelector("#numGuess").value
-
     gameText.innerHTML = "You guessed: " + numGuess;
 }
-
-{/* <label for="numGuess">Guess a Number: </label>
-<input type="text" id="numGuess" name="numGuess">
-<input type="button" value="Guess" onclick="logged()"></input> */};
