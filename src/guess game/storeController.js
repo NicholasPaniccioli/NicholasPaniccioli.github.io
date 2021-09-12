929ac7d2f6dc6be705d066031f1a6cfaf2f37a1f
NicholasPaniccioli <njp7745@g.rit.edu>
2021-09-12 14:31:07 -0400

    1.5.1 - Shop Fixed, Other Misc
    Error with buttons not properly changing onclick and thus not changing colors. Also forced players to buy previously bought store items. Created new row for unlocked buttons to go around this issue. Logic to display and hide correct buttons added. Also CSS fixes for the tabs, and quality of life like Point display in store

--- src/guess game/storeController.js
+++ src/guess game/storeController.js
@@ -9,28 +9,44 @@ else{
 
 //Changes background color of the game
 function changeBackground(value) {
-    let gameBack = document.querySelector("#gameTextDiv");
+    let gameBack = document.querySelector("#gameFeedback");
     gameBack.style.backgroundColor = value;
 }
 
 //Unlocks selected color
 function unlockColor(color){
-    let colorButtons = document.getElementsByClassName("colorButton");
+    let colorButton = document.getElementsByClassName("colorButton");
+    let boughtColor = document.getElementsByClassName("boughtColor")
     let points = parseInt(localStorage.getItem("Points"))
 
     //loops through and matches color with name
-    for(let i = 0; i < colorButtons.length; i++) {
-        let col = colorButtons[i].getAttribute("name");
+    for(let i = 0; i < colorButton.length; i++) {
+        let col = colorButton[i].getAttribute("name");
 
         if(col == color) //Checks color matches
         {
             if(checkUnlock(points, 5)) //Checks if player has enough points
             {
                 subtractPoints(points, 5); //Spends points for the color
-                colorButtons[i].value = color;
                 unlockedColors.push(color);
                 localStorage.setItem("UnlockedColors", JSON.stringify(unlockedColors)); //Updates local storage
-                colorButtons[i].addEventListener("click", changeBackground(color)); //'unlocks' and gives it option to change color
+
+                //Loops through to unlock the bought color 
+                for(let x = 0; x < boughtColor.length; x++) {
+                    if(col == boughtColor[x].name) {
+                        boughtColor[x].style.display = "inline";
+                        break;
+                    }
+                }
+
+                //Loops through to disable buy button
+                for(let z = 0; z < colorButton.length; z++) {
+                    if(col == colorButton[z].name) {
+                        colorButton[z].style.display = "none"; //If the color is unlocked, hides the buy button
+                        break;
+                    }
+                }
+                
             }
             else{
                 console.log("Not Enough Points")
@@ -40,18 +56,32 @@ function unlockColor(color){
 }
 
 //Unlocks already bought and unlocked colors
+//Hides buy buttons
 function unlockBoughtColors(){
-    let colorButtons = document.getElementsByClassName("colorButton");
+    let unlCol = JSON.parse(localStorage.getItem("UnlockedColors"));
+    let colorButton = document.getElementsByClassName("colorButton");
+    let boughtColor = document.getElementsByClassName("boughtColor");
+    console.log(unlCol[0]);
 
-    //loops through color buttons
-    for(let i = 0; i < colorButtons.length; i++) {
-        let col = colorButtons[i].getAttribute("name");
+    //loops through bought buttons
+    for(let i = 0; i < unlCol.length; i++) {
+        let col = unlCol[i];
         //Checks if it is unlocked
-        if(unlockedColors.includes(col))
-        {
-            //If so unlocks it again
-            colorButtons[i].value = col;
-            colorButtons[i].addEventListener("click", changeBackground(col));
+        for(let x = 0; x < boughtColor.length; x++) {
+            if(col == boughtColor[x].name) {
+                boughtColor[x].style.display = "inline"; //If its in the unlocked array displays it
+            }
+        }
+    }
+
+    //Loops through the color buttons
+    for(let y = 0; y < unlCol.length; y++) {
+        let col = unlCol[y];
+        //Checks if it is unlocked
+        for(let z = 0; z < colorButton.length; z++) {
+            if(col == colorButton[z].name) {
+                colorButton[z].style.display = "none"; //If the color is unlocked, hides the buy button
+            }
         }
     }
 }
@@ -63,6 +93,8 @@ function subtractPoints(playerPoints, subtract) {
         points = playerPoints - subtract;
         localStorage.setItem("Points", points);
     }
+
+    showPoints(); //When points are used, updates display
 }
 
 //Helper function to see if unlock should go through.
