6a552041448c836631bcbe1ad8a47a8081944496
NicholasPaniccioli <nicholaspanic20@gmail.com>
2022-01-17 20:10:54 -0500

    New Images, More Projects!
    More Projects have been added! The base template page has been given for each, with a picture too. Also organized and redid some HTMl/CSS. JS was changed to work with the new pages.

--- src/projectHandler.js
+++ src/projectHandler.js
@@ -1,27 +1,45 @@
 //Gives back the appropriate info based on selected project
 function getProject (proj){
-    // Grabs all the projects in the list
+    // Grabs all the projects in the list, the container for the thumbnail, and the view button
     let projList = document.getElementById('projList');
     let projPic = document.getElementById('projPic');
+    let viewBtn = document.getElementById('viewBtn');
+
+    //By default gives aleart saying there is no view page for a given project
+    viewBtn.onclick = function(){alert("Hi there! Unfortunately there's no site to view this work. This may be because"
+    + " it's being worked on currently or the product itself is on a different platform.");}
 
     //Hamburger button
     projList.innerHTML = "&#9776";
-    //Finds the project selected and gives documentation
+    //Finds the project selected and gives documentation, thumbnail, and appropriate view link (if one is given)
     //PROJECTS IN ALPHABETICAL ORDER
     if(proj === "GTN") {
         displayDocumentation("guessTheNumber");
         projList.innerHTML += "  Guess The Number";
-        projPic.src = "../images/guess game/guessGame.jpg";
+        projPic.src = "../images/projects/GTN/GTN-Thumb.JPG";
+        viewBtn.onclick = function() {window.open('game.html','_blank')};
     } else if(proj === "LF") {
         //Landmark Finder
         displayDocumentation("landmarkFinder");
         projList.innerHTML += "  Landmark Finder";
-        projPic.src = "../images/portfolio/mapboxAPI.JPG";
+        projPic.src = "../images/projects/LF/LF-Thumb.jpeg";
+        viewBtn.onclick = function(){window.open('https://people.rit.edu/njp7745/330/exercises/Project-3/','_blank')};
     } else if(proj === "OSE") {
         //Outer Space Experience
         displayDocumentation("outerSpaceExperience");
         projList.innerHTML += "  Outer Space Experience";
-        projPic.src = "../images/portfolio/spaceExp.JPG";
+        projPic.src = "../images/projects/OSE/OSE-Thumb.jpeg";
+        viewBtn.onclick = function(){window.open('https://people.rit.edu/njp7745/330/exercises/Project-2/','_blank')};
+    } else if(proj === "PORT") {
+        //VR Convention
+        displayDocumentation("portfolioSite");
+        projList.innerHTML += "  My Portfolio Site";
+        projPic.src = "../images/projects/PORT/Home-A.jpeg";
+    } else if(proj === "VRC") {
+        //VR Convention
+        displayDocumentation("vrConvention");
+        projList.innerHTML += "  VR Convention";
+        projPic.src = "../images/projects/VRC/Convention-A.JPG";
     } else {
         //If none is found, default message
         alert("Sorry, was unable to find that project. Loading Default!");
